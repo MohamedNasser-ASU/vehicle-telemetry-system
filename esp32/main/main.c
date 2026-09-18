3f251405465ec3dfc389b310f213d32edda99938
Mohamed Nasser <mohamednasser12018@gmail.com>
2026-09-18 19:04:38 +0300

    got the motor moving

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_err.h"
 #include "hal/adc_types.h"
+#include "hal/ledc_types.h"
+
+
 
 
 #define LED GPIO_NUM_2
@@ -30,12 +36,48 @@ void app_main(void){
     .atten = ADC_ATTEN_DB_12,
     };
 
-    // configure channels 6 and 7 because i chose pins 34 and 45
+    // configure channels 6 and 7 because i chose pins 34 and 35
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_6, &config));
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_7, &config));
     
+        
+    // configure PWM for motor 1
+    ledc_timer_config_t pwm = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .duty_resolution = LEDC_TIMER_8_BIT,
+        .timer_num = LEDC_TIMER_0,
+        .freq_hz = 20000,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&pwm));
+
+    // configure PWM channel for motor 1
+    ledc_channel_config_t pwmChannel0 = {
+        .gpio_num = 25,
+        .speed_mode= LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&pwmChannel0));
+
+    ledc_channel_config_t pwmChannel1 = {
+        .gpio_num = 26,
+        .speed_mode= LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_1,
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&pwmChannel1)); 
+    
 
     while (1){
+
+    
 
         // read adc1 raw analog output
         int adc1_VRx;
@@ -79,6 +121,30 @@ void app_main(void){
         
         if (digital_VRy >= -3 && digital_VRy <= 3)
         digital_VRy = 0;
+
+        // Control pwm with joystick
+        uint32_t duty;
+        if ( digital_VRy > 0) {
+            // forward
+            duty = digital_VRy * 255 / 100;
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty);
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        }
+        else if ( digital_VRy < 0){
+            // backward
+            duty = abs(digital_VRy) * 255 / 100;
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0);
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        }
+        else {
+            // stop
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0);
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        }
+
 
         // print value
         vTaskDelay(pdMS_TO_TICKS(200));
